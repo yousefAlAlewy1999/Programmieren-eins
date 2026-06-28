a7984f3567d293e6f50532640a59513886936a33
yousefAlAlewy1999 <137657013+yousefAlAlewy1999@users.noreply.github.com>
2026-06-28 04:05:42 +0200

    Kommentiert

--- src/manager/BuchungsManager.java
+++ src/manager/BuchungsManager.java
@@ -10,84 +10,129 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BuchungsManager {
-//Yousef
-    private List<Raum>    raeume;
-    private List<Buchung> buchungen;
-    private int           naechsteBuchungsId;
+// Yousef
 
+    // Liste aller Räume im System
+    private List<Raum> raeume;
+
+    // Liste aller Buchungen im System
+    private List<Buchung> buchungen;
+
+    // Laufende ID für neue Buchungen
+    private int naechsteBuchungsId;
+
+    // Konstruktor: Initialisiert die Listen und setzt die erste Buchungs-ID
     public BuchungsManager() {
         this.raeume             = new ArrayList<>();
         this.buchungen          = new ArrayList<>();
         this.naechsteBuchungsId = 1;
     }
 
+    // Getter für alle Räume
     public List<Raum> getRaeume() {
         return raeume;
     }
 
+    // Getter für alle Buchungen
     public List<Buchung> getBuchungen() {
         return buchungen;
     }
 
+    // Legt einen neuen Raum an
     public void raumAnlegen(String name, int kapazitaet, String ausstattung,
                             Zeitslot oeffnungszeitfenster) {
+
+        // Prüft, ob der Raum am gleichen Datum bereits existiert
         for (Raum r : raeume) {
             if (r.getName().equals(name)
                     && r.getOeffnungszeitfenster().getDatum().equals(oeffnungszeitfenster.getDatum())) {
+
+                // Fehler, wenn Raum doppelt existiert
                 throw new BuchungsException(
                     "Raum '" + name + "' existiert am " + oeffnungszeitfenster.getDatum()
                     + " bereits.");
             }
         }
 
+        // Neue Raum-ID finden
         int neueId = findeNaechsteFreieRaumId();
+
+        // Raum erzeugen
         Raum neuerRaum = new Raum(neueId, name, kapazitaet,
                                    ausstattung, oeffnungszeitfenster);
+
+        // Raum zur Liste hinzufügen
         raeume.add(neuerRaum);
+
         System.out.println("Raum '" + name + "' wurde erfolgreich angelegt.");
     }
 
+    // Löscht einen Raum anhand seiner ID
     public void raumLoeschen(int raumId) {
+
+        // Raum suchen
         Raum zuLoeschen = findeRaum(raumId);
+
+        // Raum entfernen
         raeume.remove(zuLoeschen);
-        
+
+        // Alle Buchungen dieses Raumes entfernen
         buchungen.removeIf(b -> b.getRaum().getRaumId() == raumId);
+
         System.out.println("Raum #" + raumId + " wurde erfolgreich gelöscht.");
     }
 
+    // Erstellt eine neue Buchung
     public Buchung buchungErstellen(Student student, int raumId, Zeitslot zeitslot) {
+
+        // Raum anhand ID finden
         Raum raum = findeRaum(raumId);
 
+        // Prüfen: Liegt der Zeitslot innerhalb der Öffnungszeit?
         if (!raum.liegtImOeffnungszeitfenster(zeitslot)) {
             throw new BuchungsException(
                 "Der gewünschte Zeitslot liegt außerhalb der Öffnungszeit von Raum '"
                 + raum.getName() + "' (" + raum.getOeffnungszeitfenster() + ").");
         }
 
+        // Prüfen: Hat der Raum noch freie Plätze?
         if (!raum.isVerfuegbar()) {
             throw new BuchungsException(
                 "Raum '" + raum.getName() + "' hat keinen Platz mehr.");
         }
 
+        // Prüfen: Hat der Student diesen Zeitslot bereits gebucht?
         for (Buchung b : buchungen) {
             if (b.getStudent().getStudentId() == student.getStudentId()
                     && b.getRaum().getRaumId() == raumId
                     && b.getZeitslot().ueberschneidetSich(zeitslot)) {
+
                 throw new BuchungsException(
                     "Sie haben diesen Zeitslot bereits gebucht.");
             }
         }
 
+        // Kapazität des Raumes verringern
         raum.kapazitaetVerringern();
 
+        // Neue Buchung erzeugen
         Buchung neueBuchung = new Buchung(
             naechsteBuchungsId, student, raum, zeitslot);
+
+        // Buchung dem Raum hinzufügen
         raum.getBuchungen().add(neueBuchung);
+
+        // Buchung der globalen Liste hinzufügen
         buchungen.add(neueBuchung);
+
+        // Buchungs-ID erhöhen
         naechsteBuchungsId++;
+
         System.out.println("Buchung erfolgreich: " + neueBuchung);
+
         return neueBuchung;
     }
+}
 	//Yousef
 //Aldrin
     public void buchungStornieren(int buchungsId) {
